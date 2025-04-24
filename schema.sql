e54b9b355e1e6e06f41610ae8306337a39c51106
APOORVA86 <ramapoorva2005@gmail.com>
2025-04-24 14:08:25 +0530

    text, entities,keywords tables

--- schema.sql
+++ schema.sql
@@ -5,3 +5,26 @@ CREATE TABLE IF NOT EXISTS transcripts (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE processed_text (
+    processed_text_id INT AUTO_INCREMENT PRIMARY KEY,
+    transcript_id INT,
+    preprocessed_text TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (transcript_id) REFERENCES transcripts(transcript_id)
+);
+
+CREATE TABLE entities (
+    entity_id INT AUTO_INCREMENT PRIMARY KEY,
+    processed_text_id INT,
+    entity_text VARCHAR(255),
+    entity_type VARCHAR(255),
+    FOREIGN KEY (processed_text_id) REFERENCES processed_text(processed_text_id)
+);
+
+CREATE TABLE keywords (
+    keyword_id INT AUTO_INCREMENT PRIMARY KEY,
+    processed_text_id INT,
+    keyword VARCHAR(255),
+    relevance FLOAT,
+    FOREIGN KEY (processed_text_id) REFERENCES processed_text(processed_text_id)
+);
